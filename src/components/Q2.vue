8dbd2b8b71d8675384209d4f86dce803913bad71
yii.zhang <yii.zhang@sigmob.com>
2026-03-11 20:21:41 +0800

    feat: 调整q2

--- src/components/Q2.vue
+++ src/components/Q2.vue
@@ -8,36 +8,22 @@
 </template>
 
 <script setup lang="ts">
-const imgs = [
-  'https://appimg.dbankcdn.com/application/icon144/ef3e86d75d6d44e78486cf4696ba4817.png',
-  'https://appimg.dbankcdn.com/application/icon144/91771a4c7b38427db0924bcbcc601c0d.png',
-  'https://appimg.dbankcdn.com/application/icon144/359077f2e88147bb81ce5cc8b420230c.png',
-  'https://appimg.dbankcdn.com/application/icon144/154b3bc2c59a4a09b5dc8841b70eb47b.png',
-  'https://appimg.dbankcdn.com/application/icon144/b1148afa90704b59b9cff9baa7749bda.png',
-  'https://appimg.dbankcdn.com/application/icon144/b6fea3f0d91b450992466c8283aa17b3_1.png',
-  'https://appimg.dbankcdn.com/application/icon144/af253cd113864ae5ba9093534e71066e.png',
-  'https://appimg.dbankcdn.com/application/icon144/f81a5f8e63a84e9b87332c2e00ad867d.png',
-  'https://appimg.dbankcdn.com/application/icon144/6c5a95b128c540e3be1792ecb226bf44.png',
-  'https://appimg.dbankcdn.com/application/icon144/24c0605971da428b8681a9b388644eea.png',
-];
+const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// 实现一个方法，加载指定url的图片，要求返回一个promise
-function loadImg(url: string): Promise<void> {
+// 获取数据
+function getUser(id: number): Promise<string> {
   return new Promise((resolve, reject) => {
-    const imageNode = new Image();
-    imageNode.src = url;
-    imageNode.onload = () => resolve();
-    imageNode.onerror = reject;
+    setTimeout(() => resolve(`用户 - ${id}`), id % 2 ? 4000 : 1000)
   });
 }
 
 function delay(delayTime: number): Promise<void> {}
 
-// 有n张图片url，要求实现一个方法，可以依次加载这些图片，第一个加载完成后才加载第二个
-async function loadImgs(urls: string[]): Promise<void> {}
-loadImgs(imgs);
-// 在图片加载时，设定一个超时时间3秒，比如某一张图片如果加载超过3秒，则不再等待，直接去加载下一张图片。该如何实现
-async function loadImgWithTimeout(urls: string[], delayTime) {}
+// 要求实现一个方法，可以依次获取用户信息，第一个获取后再获取第二个
+async function getUsers(ids: number[]): Promise<void> {}
+getUsers(ids);
+// 在数据获取时，设定一个超时时间3秒，比如某一个用户信息获取如果加载超过3秒，则不再等待，直接去获取下一个用户信息。该如何实现
+async function getUserWithTimeout(ids: number[], delayTime) {}
 </script>
 
 <style scoped>
